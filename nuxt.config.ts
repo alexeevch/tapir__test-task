9eb77a4481d0aa5d83a80e3732f6589850590d03
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2026-02-26 14:23:55 +0300

    feat: Обновлен nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,29 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+
+  modules: ["@nuxt/eslint", "nuxt-svgo"],
+
+  runtimeConfig: {
+    public: {
+      appName: "",
+      apiUrl: ""
+    }
+  },
+
+  css: ["~/assets/styles/main.scss"],
+
+  svgo: {
+    autoImportPath: "~/assets/icons/"
+  },
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "~/assets/styles/functions" as *;`
+        }
+      }
+    }
+  }
+});
