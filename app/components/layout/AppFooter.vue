2b9ae52f64d37a65c723ec546b203a60c3936235
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2026-02-26 14:26:41 +0300

    feat: Подвал сайта

---
+++ app/components/layout/AppFooter.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import MenuList from "~/components/ui/menu/menu-list.vue";
+
+const { menu } = useAppConfig();
+</script>
+
+<template>
+  <footer class="footer">
+    <div class="container">
+      <MenuList :items="menu.footerMenu" />
+    </div>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+@use "~/assets/styles/mixins" as mixins;
+
+.footer {
+  background-color: var(--color-black);
+  color: var(--color-white);
+  padding: pxToRem(100) 0;
+
+  @include mixins.screensFrom("sm") {
+    padding: pxToRem(60) 0;
+  }
+}
+</style>
