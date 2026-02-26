516d82a6391b265e9a0e035a2311d7ec10afc898
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2026-02-26 14:26:22 +0300

    feat: Базовый компонент логотипа

---
+++ app/components/layout/AppLogo.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import LogoPrimaryIcon from "~/assets/icons/logo-primary-icon.svg";
+</script>
+
+<template>
+  <LogoPrimaryIcon class="logo" />
+</template>
+
+<style scoped lang="scss">
+@use "~/assets/styles/mixins" as mixins;
+
+.logo {
+  font-size: pxToRem(42);
+  color: var(--color-red);
+
+  @include mixins.screensFrom("sm") {
+    font-size: pxToRem(28);
+  }
+}
+</style>
