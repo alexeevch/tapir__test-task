7d2eb7ab0cbc874fcaf8dbaf9a166dac163eae6c
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2026-02-26 14:28:27 +0300

    feat: Каталог кроссовок

---
+++ app/pages/catalog/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import ProductSection from "~/components/catalog/ProductSection.vue";
+
+const config = useRuntimeConfig().public;
+
+useSeoMeta({
+  title: `Каталог - ${config.appName}`
+});
+</script>
+
+<template>
+  <div class="catalog">
+    <h1 class="catalog__heading page-heading">Каталог</h1>
+    <ProductSection />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.catalog {
+  &__heading {
+    text-align: center;
+  }
+}
+</style>
