8df10aa49b45c3a745895770040d6d7d3a8efc40
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2026-02-26 14:27:20 +0300

    feat: Реализация списка карточек продукта

---
+++ app/components/catalog/ProductList.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import type { ProductDTO } from "~/types/api/product";
+import ProductCard from "~/components/catalog/ProductCard.vue";
+
+const { items } = defineProps<{ items: ProductDTO[] }>();
+</script>
+
+<template>
+  <div class="products">
+    <div v-for="(item, key) in items" :key class="products__item">
+      <ProductCard :product="item" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use "~/assets/styles/mixins" as mixins;
+
+.products {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: pxToRem(40);
+
+  @include mixins.screensFrom("xl") {
+    grid-template-columns: repeat(3, 1fr);
+    gap: pxToRem(30);
+  }
+
+  @include mixins.screensFrom("md") {
+    grid-template-columns: repeat(2, 1fr);
+    gap: pxToRem(20);
+  }
+}
+</style>
